e93fef02ed21648ef36e019def3bd0070c5b682f
Samuel2024-27 <jaajperoquepasa@gmail.com>
2024-11-12 20:09:46 -0600

    Update Administrador.java

--- src/main/java/FixItUp/FixItUp/Entidad/Administrador.java
+++ src/main/java/FixItUp/FixItUp/Entidad/Administrador.java
@@ -3,5 +3,39 @@ package FixItUp.FixItUp.Entidad;
 public class Administrador {
 
     
+    private String nombre;
+    private String password;
 
 }
+public Administrador () {
+
+}
+
+    public Administrador (String nombre, String password) {
+        this.nombre = nombre;
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }

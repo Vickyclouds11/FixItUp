b6aef925faa79cbc29358da286f47f77fe6e69cd
Samuel2024-27 <jaajperoquepasa@gmail.com>
2024-11-12 21:26:49 -0600

    Update repositorio.java

--- src/main/java/FixItUp/FixItUp/Repositorios/repositorio.java
+++ src/main/java/FixItUp/FixItUp/Repositorios/repositorio.java
@@ -1,4 +1,32 @@
 package FixItUp.FixItUp.Repositorios;
 
-public interface repositorio {
+
+
+import Entidad.Administrador;
+import java.util.ArrayList;
+import java.util.List;
+
+public class repositorio {
+    private List<Administrador> administradores = new ArrayList<>();
+
+    public List<Administrador> findAll() {
+        return administradores;
+    }
+
+    public void save(Administrador administrador) {
+        administradores.add(administrador);
+    }
+
+    public Administrador findById(Long id) {
+        for (Administrador admin : administradores) {
+            if (admin.getId().equals(id)) {
+                return admin;
+            }
+        }
+        return null;
+    }
+
+    public boolean deleteById(Long id) {
+        return administradores.removeIf(admin -> admin.getId().equals(id));
+    }
 }

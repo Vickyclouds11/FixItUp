9619c15378820278c073fcb55e747c34002951c8
Samuel2024-27 <jaajperoquepasa@gmail.com>
2024-11-12 20:13:08 -0600

    Update Servicio.java

--- src/main/java/FixItUp/FixItUp/Servicios/Servicio.java
+++ src/main/java/FixItUp/FixItUp/Servicios/Servicio.java
@@ -1,4 +1,28 @@
 package FixItUp.FixItUp.Servicios;
 
 public class Servicio {
+
+    ArrayList<Administrador> lista = new ArrayList<>();
+
+    public Servicio() {
+        lista.add(new Administrador("Gerardo","daSDJSDJ203"));
+        lista.add(new Administrador("Alfredo","DASFMsas32"));
+        lista.add(new Administrador("Maria","D3DNass34dsd"));
+        lista.add(new Administrador("Julian","ODSsdme392"));
+    }
+
+    public ArrayList<Administrador> getContraseña(){
+        return lista;
+    }
+
+    public Administrador agregarContraseña(Administrador p){
+        lista.add(p);
+        return p;
+    }
+
+    public boolean eliminarContraseña(String nombre) {
+        return lista.removeIf(contraseña -> contraseña.getNombre().equalsIgnoreCase(nombre));
+    }
+
 }
+

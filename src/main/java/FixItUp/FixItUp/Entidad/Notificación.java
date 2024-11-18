72ed97cfdd761454191c63a7c9e3e4b8a7c012aa
AnaaBlanc0 <anamablanco24@gmail.com>
2024-11-18 17:29:07 -0500

    Update Notificación.java

--- src/main/java/FixItUp/FixItUp/Entidad/Notificación.java
+++ src/main/java/FixItUp/FixItUp/Entidad/Notificación.java
@@ -1,1 +1,19 @@
+package FixItUp.FixItUp.Entidad;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+public class Notificacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String contenido;
+
+    @Column(nullable = false)
+    private LocalDate fechaEnvio;
 

97d5af7315dc9e83d09f470364e5f925b0f6e8a7
AnaaBlanc0 <anamablanco24@gmail.com>
2024-11-18 16:18:51 -0500

    Update RepositoryC.java

--- src/main/java/FixItUp/FixItUp/Repositorios/RepositoryC.java
+++ src/main/java/FixItUp/FixItUp/Repositorios/RepositoryC.java
@@ -1,1 +1,12 @@
+package FixItUp.FixItUp.Repositorios;
+
+import FixItUp.FixItUp.Entidad.Consumidor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+@org.springframework.stereotype.Repository
+public interface RepositoryC extends JpaRepository<Consumidor, String> {
+
+}
+
 

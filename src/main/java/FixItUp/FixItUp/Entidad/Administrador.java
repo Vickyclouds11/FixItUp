6d129a2f48f6343ccf7070799cca52479162196a
Victoria <73242613+Vickyclouds11@users.noreply.github.com>
2024-11-18 15:50:58 -0500

    Update Administrador.java

--- src/main/java/FixItUp/FixItUp/Entidad/Administrador.java
+++ src/main/java/FixItUp/FixItUp/Entidad/Administrador.java
@@ -1,21 +1,36 @@
 package FixItUp.FixItUp.Entidad;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
 public class Administrador {
-    private Long id;
+
+    @Id
+    private String id;
+
+    @Column(nullable = false, length = 50)
     private String usuario;
+
+    @Column(nullable = false, length = 50)
     private String contraseña;
-    private static Long contadorId = 1L;
+
+    public Administrador() {
+
+    }
 
     public Administrador(String usuario, String contraseña) {
-        this.id = contadorId++;
         this.usuario = usuario;
         this.contraseña = contraseña;
     }
 
-   
-    public Long getId() {
+    public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getUsuario() {
@@ -32,5 +47,14 @@ public class Administrador {
 
     public void setContraseña(String contraseña) {
         this.contraseña = contraseña;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrador{" +
+                "id=" + id +
+                ", usuario='" + usuario + '\'' +
+                ", contraseña='" + contraseña + '\'' +
+                '}';
     }
 }

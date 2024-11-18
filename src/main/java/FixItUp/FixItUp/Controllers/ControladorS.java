437e8877baa821b7442ac2a73da8a596b17477de
Victoria <73242613+Vickyclouds11@users.noreply.github.com>
2024-11-18 17:41:55 -0500

    Create ControladorS.java

---
+++ src/main/java/FixItUp/FixItUp/Controllers/ControladorS.java
@@ -0,0 +1,71 @@
+package FixItUp.FixItUp.Controllers;
+import FixItUp.FixItUp.Entidad.Servicio;
+import FixItUp.FixItUp.Servicios.ServicioS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@CrossOrigin("http://127.0.0.1:5501")
+@RestController
+public class ControladorS {
+
+    private final ServicioS servicio;
+    private boolean sesionIniciada;
+
+
+    @Autowired
+    public ControladorS(ServicioS servicio) {
+        this.servicio = servicio;
+        this.sesionIniciada = false;
+    }
+
+    @PostMapping("/iniciar-sesionn")
+    public String iniciarSesion(@RequestParam String usuario, @RequestParam String contraseña) {
+        if (servicio.login(usuario, contraseña)) {
+            sesionIniciada = true;
+            return "Inicio de sesión exitoso.";
+        } else {
+            return "Error en el inicio de sesión.";
+        }
+    }
+
+    @GetMapping("/Serv")
+    public List<Servicio> listarUsuarios() {
+        return servicio.listarUsuarios();
+    }
+
+    @PostMapping("/Post/Serv")
+    public Servicio agregar(@RequestBody Servicio serv) {
+        return servicio.agregarUsuario(serv);
+    }
+
+    /*@PutMapping("/usuarios/{id}")
+    public void actualizarUsuario(@PathVariable String id, @RequestParam String usuarioActualizado, @RequestParam String contraseñaActualizada) {
+        if (sesionIniciada) {
+            boolean actualizado = servicio.actualizarUsuario(id, usuarioActualizado, contraseñaActualizada);
+            if (actualizado) {
+                System.out.println("Usuario actualizado.");
+            } else {
+                System.out.println("Usuario no encontrado.");
+            }
+        } else {
+            System.out.println("Debe iniciar sesión primero.");
+        }
+    }*/
+
+    /*@DeleteMapping("/usuarios/{id}")
+    public void eliminarUsuario(@PathVariable String id) {
+        if (sesionIniciada) {
+            boolean eliminado = servicio.eliminarUsuario(id);
+            if (eliminado) {
+                System.out.println("Usuario eliminado.");
+            } else {
+                System.out.println("Usuario no encontrado.");
+            }
+        } else {
+            System.out.println("Debe iniciar sesión primero.");
+        }
+    }*/
+}

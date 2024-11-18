8da840ec3902e5673f73957155a858cb03425d5a
Victoria <73242613+Vickyclouds11@users.noreply.github.com>
2024-11-18 16:08:56 -0500

    Update RepositoryV.java

--- src/main/java/FixItUp/FixItUp/Repositorios/RepositoryV.java
+++ src/main/java/FixItUp/FixItUp/Repositorios/RepositoryV.java
@@ -1,1 +1,9 @@
+package FixItUp.FixItUp.Repositorios;
+import FixItUp.FixItUp.Entidad.Vendedor;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+
+@org.springframework.stereotype.Repository
+public interface RepositoryV extends JpaRepository<Vendedor, String> {
+
+}
